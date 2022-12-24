f333074e003ef42fe4ce0abbe32aa11803fbcdfb
Jeff-Salvant <salvant3jeff@gmail.com>
2022-12-24 11:53:54 -0500

    Create basic layout of home page

---
+++ shopping-cart-project/src/components/HomePage/HomePage.js
@@ -0,0 +1,21 @@
+function HomePage() {
+    return (
+        <div>
+            <div className="unit-copy-wrapper">
+                <h2 className="headline">Messi Apparel</h2>
+                <h3 className="sub-headline">World champion</h3>
+                <div className="cta-link">
+                    <a href="/">Shop now</a>
+                    <div>icon</div>
+                </div>
+            </div>
+            <div className="unit-image-wrapper">
+                <figure className="unit-image">
+                    
+                </figure>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
