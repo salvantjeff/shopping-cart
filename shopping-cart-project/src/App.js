f40537f7c28d9309a3d3c27820f76ae47919201a
Jeff-Salvant <salvant3jeff@gmail.com>
2022-12-26 11:17:40 -0500

    Create basic cart page and with basic style

--- shopping-cart-project/src/App.js
+++ shopping-cart-project/src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar/NavBar';
 import { useState, useEffect } from 'react';
 import Menu from './components/Menu/Menu';
 import SearchPage from './components/SearchPage/SearchPage';
+import CartPage from './components/CartPage/CartPage';
 
 function App() {
   const [navHeight, setNavHeight] = useState(0);
@@ -17,7 +18,7 @@ function App() {
       <div className='unit-wrapper' style={{marginTop: `${navHeight}px`}} >
         <NavBar navHeight={navHeight}/>
         <HomePage navHeight={navHeight}/>
-        <SearchPage />
+        <CartPage />
       </div>
     </div>
   );
---
+++ shopping-cart-project/src/components/CartPage/CartPage.js
@@ -0,0 +1,36 @@
+import './CartPage.css';
+import { GrClose } from 'react-icons/gr';
+
+function CartPage() {
+    return (
+        <div className="cart-page">
+            <div className="cart-wrapper">
+                <div className="cart-header">
+                    <h3>Your Shopping Cart</h3>
+                    <button className='close-button'><GrClose /></button>
+                </div>
+                <div className='cart-content'>
+                    {/* <ul className="cart-items">
+                        <li className='cart-item'>Messi Jersey</li>
+                        <li className='cart-item'>Messi world cup cleats</li>
+                        <li className='cart-item'>Messi ball</li>
+                    </ul> */}
+                    <div>
+                        <p className='empty-cart-message'>Your bag is empty</p>
+                    </div>
+                </div>
+                <div className='cart-checkout-footer'>
+                    <div className='cart-total-wrapper'>
+                        <p className='cart-total-label'>Total</p>
+                        <p className='cart-total-price'>$0.00</p>
+                    </div>
+                    <a href='/' className='checkout-link'>
+                        <div className='checkout-button'>Checkout</div>
+                    </a>
+                </div>
+            </div>
+        </div>  
+    );
+};
+
+export default CartPage;
