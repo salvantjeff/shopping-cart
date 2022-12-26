cdca4ce616562a37d902e520a88c0a649fc34da4
Jeff-Salvant <salvant3jeff@gmail.com>
2022-12-26 17:38:11 -0500

    Fix overflow-y

--- shopping-cart-project/src/components/ShopPage/ShopPage.js
+++ shopping-cart-project/src/components/ShopPage/ShopPage.js
@@ -9,19 +9,19 @@ import ShopProductCard from './ShopProductCard/ShopProductCard';
 //         imgURL: '',
 //     }
 // ];
-function ShopPage() {
+function ShopPage({ navHeight }) {
     return (
-        <div className='shop-page'>
+        <div className='shop-page' >
             <div className='shop-page-wrapper'>
                 <div className='shop-header'>
                     <h3>All items (25)</h3>
                 </div>
                 <div className='shop-categories'>
-                    <div>Tops</div>
-                    <div>Bottoms</div>
-                    <div>Shoes</div>
-                    <div>Accessories</div>
-                </div>
+                    <div className='category'>Tops</div>
+                    <div className='category'>Bottoms</div>
+                    <div className='category'>Shoes</div>
+                    <div className='category'>Accessories</div>
+                </div> 
                 <div className='product-cards'>
                     <ShopProductCard />
                     <ShopProductCard />
