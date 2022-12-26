6a3c7a1416011e5b73b6c3fdcd05de50ab888a5e
Jeff-Salvant <salvant3jeff@gmail.com>
2022-12-26 13:15:33 -0500

    Extract product card to create its own component

--- shopping-cart-project/src/components/ShopPage/ShopPage.js
+++ shopping-cart-project/src/components/ShopPage/ShopPage.js
@@ -1,10 +1,14 @@
 import './ShopPage.css';
+import ShopProductCard from './ShopProductCard/ShopProductCard';
 
-const imgURL = 'https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto,fl_lossy,c_fill,g_auto/d66ee9ef09e24a1d94aeaedd00f57a08_9366/samba-argentina-shoes.jpg';
-const imgURL2 = 'https://assets.adidas.com/images/w_303,h_303,f_auto,q_auto,fl_lossy,c_fill,g_auto/82fb5a08f3674df09a32af1f00dd0be4_9366/x-speedportal-leyenda.1-firm-ground-soccer-cleats.jpg';
-const imgURL3 = 'https://assets.adidas.com/images/w_303,h_303,f_auto,q_auto,fl_lossy,c_fill,g_auto/6af8fc9d62e34d66b31baf500038c525_9366/argentina-2022-winners-tee.jpg';
-const imgURL4 = 'https://assets.adidas.com/images/w_303,h_303,f_auto,q_auto,fl_lossy,c_fill,g_auto/4eaa8ab7680c4c559b14aebc00f7087a_9366/messi-condivo-22-half-pants.jpg';
-
+// const productDetails = [
+//     {
+//         name: '',
+//         category: '',
+//         price: 0,
+//         imgURL: '',
+//     }
+// ];
 function ShopPage() {
     return (
         <div className='shop-page'>
@@ -19,46 +23,10 @@ function ShopPage() {
                     <div>Accessories</div>
                 </div>
                 <div className='product-cards'>
-                    <div className='product-card'>
-                        <div className='product-image-container'>
-                            <img className='product-image' src={imgURL} alt='product'/>
-                        </div>
-                        <div className='product-card__info'>
-                            <div className='product-card__name'>Samba Argentina Shoes</div>
-                            <div className='product-card__category'>Shoes</div>
-                            <div className='product-card__price'>$100.00</div>
-                        </div>
-                    </div>
-                    <div className='product-card'>
-                        <div className='product-image-container'>
-                            <img className='product-image' src={imgURL2} alt='product'/>
-                        </div>
-                        <div className='product-card__info'>
-                            <div className='product-card__name'>X Speedportal Leyenda.1 Firm Ground</div>
-                            <div className='product-card__category'>Shoes</div>
-                            <div className='product-card__price'>$120.00</div>
-                        </div>
-                    </div>
-                    <div className='product-card'>
-                        <div className='product-image-container'>
-                            <img className='product-image' src={imgURL3} alt='product'/>
-                        </div>
-                        <div className='product-card__info'>
-                            <div className='product-card__name'>X Speedportal Leyenda.1 Firm Ground</div>
-                            <div className='product-card__category'>Shoes</div>
-                            <div className='product-card__price'>$120.00</div>
-                        </div>
-                    </div>
-                    <div className='product-card'>
-                        <div className='product-image-container'>
-                            <img className='product-image' src={imgURL4} alt='product'/>
-                        </div>
-                        <div className='product-card__info'>
-                            <div className='product-card__name'>X Speedportal Leyenda.1 Firm Ground</div>
-                            <div className='product-card__category'>Shoes</div>
-                            <div className='product-card__price'>$120.00</div>
-                        </div>
-                    </div>
+                    <ShopProductCard />
+                    <ShopProductCard />
+                    <ShopProductCard />
+                    <ShopProductCard />
                 </div>
             </div>
         </div>
