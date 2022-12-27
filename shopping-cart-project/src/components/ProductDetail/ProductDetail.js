61c97bd4798649e4c158d066f8c60005fe025698
Jeff-Salvant <salvant3jeff@gmail.com>
2022-12-27 18:35:42 -0500

    Import useParams to get id of item clicked
    This will allow filtering of productsDetails array for targeted product

--- shopping-cart-project/src/components/ProductDetail/ProductDetail.js
+++ shopping-cart-project/src/components/ProductDetail/ProductDetail.js
@@ -1,8 +1,11 @@
 import './ProductDetail.css';
+import { useParams } from 'react-router-dom';
 
 const imgURL = 'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,b_rgb:f5f5f5/19e0246e-ba01-4900-8fc1-67e6752f0c48/air-jordan-1-retro-high-og-mens-shoes-ZdCsZM.png';
 
 function ProductDetail() {
+    const { id } = useParams();
+    console.log('--->', id);
     return (
         <div className='product-detail'>
             <div className='product-detail-wrapper'>
