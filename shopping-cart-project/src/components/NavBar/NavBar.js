d36188b86adcbafc29e3b5ebc93d451f84ce68a1
Jeff-Salvant <salvant3jeff@gmail.com>
2022-12-28 10:42:40 -0500

    Show menu dropdown if menu button clicked

--- shopping-cart-project/src/components/NavBar/NavBar.js
+++ shopping-cart-project/src/components/NavBar/NavBar.js
@@ -3,13 +3,28 @@ import { BsSearch } from 'react-icons/bs';
 import { AiOutlineMenu } from 'react-icons/ai';
 import { GrShop } from 'react-icons/gr';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function NavBar() {
+    const [hideMenu, setHideMenu] = useState(true);
+
+    if (hideMenu) {
+        document.body.classList.add('hideMenu');
+    } else {
+        document.body.classList.remove('hideMenu');
+    }
+
+    function handleMenuClicked() {
+        setHideMenu(!hideMenu);
+        console.log('you clicked on menu icon');
+    };
+
     return (
         <nav className='nav-section'>
             <ul className='nav-bar'>
-                <li className='nav-button'>
-                    <Link className='links' to='/menu'><AiOutlineMenu /></Link>  
+                <li onClick={handleMenuClicked} className='nav-button'>
+                    <AiOutlineMenu />
+                    {/* <Link className='links' to='/menu'><AiOutlineMenu /></Link>   */}
                 </li>
                 <li className='logo nav-button'>
                     <Link className='links' to='/'>M</Link>
