b99f9a9aa7a17e5d3a44ea903f3fdc06a04be73c
Jeff-Salvant <salvant3jeff@gmail.com>
2022-12-28 11:29:13 -0500

    Create basic layout of SupportPage.js

---
+++ shopping-cart-project/src/components/SupportPage/SupportPage.js
@@ -0,0 +1,13 @@
+import './SupportPage.css';
+
+function SupportPage() {
+    return (
+        <div className='support-page'>
+            <div className='support-page-wrapper'>
+                Support
+            </div>
+        </div>
+    );
+};
+
+export default SupportPage;
