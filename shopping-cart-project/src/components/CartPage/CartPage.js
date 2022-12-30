d6fb50c9c34b07b94c7b02c62b2062786e994ee3
Jeff-Salvant <salvant3jeff@gmail.com>
2022-12-30 14:49:11 -0500

    Switch rendering of cart products to state

--- shopping-cart-project/src/components/CartPage/CartPage.js
+++ shopping-cart-project/src/components/CartPage/CartPage.js
@@ -1,9 +1,24 @@
 import './CartPage.css';
 import { GrClose } from 'react-icons/gr';
 import CartItem from './CartItem/CartItem';
+import { useState } from 'react';
 
 function CartPage() {
+    const initProduct = {
+        "name": "Samba Argentina Shoes",
+        "category": "Shoes",
+        "price": 100,
+        "imgURL": "https://assets.adidas.com/images/w_600,f_auto,q_auto/2bf0242d0b83433d9b8baedd00f54ba6_9366/Samba_Argentina_Shoes_Blue_HQ7037.jpg",
+        "id": "8edaf924-4917-408b-8ed9-dfb725b40d8f"
+    };
 
+    const [cartItems, setCartItems] = useState([
+        {
+            product: initProduct,
+            quantity: 1,
+        },
+    ]);
+    
     function handleCloseCart() {
         document.body.classList.add('hideCart');
     };
@@ -18,11 +33,14 @@ function CartPage() {
                     <button onClick={handleCloseCart} className='close-button'><GrClose /></button>
                 </div>
                 <div className='cart-content'>
-                    <div className='cart-items'>
-                       <CartItem />
-                       <CartItem />
-                       <CartItem />
-                    </div>
+                    {cartItems.map(item => {
+                        return (
+                            <div key={item.product.id} className='cart-items'>
+                                <CartItem product={item.product} quantity={item.quantity}/>
+                            </div>
+                        );
+                    })}
+                   
                     {/* <div>
                         <p className='empty-cart-message'>Your bag is empty</p>
                     </div> */}
