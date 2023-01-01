09b133eb3c84f745ca504155731ff636ea2a0ed0
Jeff-Salvant <salvant3jeff@gmail.com>
2022-12-31 19:53:54 -0500

    Create basic checkout page

---
+++ shopping-cart-project/src/components/CheckoutPage/CheckoutPage.js
@@ -0,0 +1,17 @@
+import './CheckoutPage.css';
+
+function CheckoutPage() {
+    return (
+        <div className='checkout-page'>
+            <div className='checkout-page-wrapper'>
+                <div className='checkout-message'>
+                    <h3>Sorry Checkout feature is currently unavailable</h3>
+                    <p>But look forward to its addition in V2 release</p>
+                </div>
+                <p className='gratitude'>Thank you for visiting our website :)</p>
+            </div>
+        </div>
+    );
+};
+
+export default CheckoutPage;
